efff7e4d033c75bc56fab95d832c26f61db133e3
norbert.mosinski <mosinski@lean-ocean.com>
2024-06-07 17:07:44 +0200

    mobile finish without map and 10%

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         },
       },
     },
+    screens: {
+      mobile: '393px',
+      tablet: '576px',
+      laptop: '992px',
+      desktop: '1400px'
+    },
+    fontFamily: {
+      'fira-sans': ['Fira Sans'],
+      'nuito-sans': ['Nuito Sans'],
+    },
   },
   plugins: [],
 }
